8d1bee7cfe371ed77df2b1c3df8bd98e4339c79f
zhang-xiao-miao <1286874200@qq.com>
2018-03-06 18:02:22 -0500

    then callback function basic

---
+++ promise/2.Promise.prototype.then.basic.js
@@ -0,0 +1,20 @@
+/**
+* Promise.prototype.then
+**/
+const p1 = new Promise((resolve, reject) => {
+  resolve(1)
+})
+
+// p2 is a new Promise instance
+const p2 = p1.then((data) => {
+  console.log(data)
+  return `data returned from p1's then callback function`
+})
+console.log(`p1 === p2: ${p1 === p2}`)
+
+//param of p2's callbackfunction comes from returned data from p1
+p2.then((data)  => {
+  console.log(data)
+})
+
+
