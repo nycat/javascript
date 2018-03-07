68adc960b1edef1343b4791e8306efffd8389340
zhang-xiao-miao <1286874200@qq.com>
2018-03-07 17:06:19 -0500

    const modifier

---
+++ let_const_var/3.const.js
@@ -0,0 +1,13 @@
+/**Use const to declare a constant varible
+* must assign a value with const deceleration
+**/
+const PI = 3.1415
+console.log(PI)
+
+try {
+  // once a varible is declared as a constant in es6, it can not be modified
+  PI = 10
+} catch(e) {
+  // statements
+  console.log(e)
+}
