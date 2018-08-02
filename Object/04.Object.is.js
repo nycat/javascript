6c46e7185396821370e9e481e4e88b2da1957230
zhang-xiao-miao <1286874200@qq.com>
2018-08-01 23:05:03 -0400

    Object.is

---
+++ Object/04.Object.is.js
@@ -0,0 +1,6 @@
+// es2015 standard supported
+// to test equal or not without transfer data type
+
+console.log(`null is equal to null: ${Object.is(null, null)}`)
+
+// reference: https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/Object/is
