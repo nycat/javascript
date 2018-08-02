75310498a92ea49f767e81a64f29fdc335d02927
zhang-xiao-miao <1286874200@qq.com>
2018-08-01 23:00:04 -0400

    Object.create usage

--- Object/03.Object.create.js
+++ Object/03.Object.create.js
@@ -1,33 +1,53 @@
 var proto = {
   saySth: function (sth) {
     console.log(sth)
-    return sth
   },
   sayHi: function () {
     var sth = 'Hi'
     console.log(sth)
-    return sth
   }
 }
 
 // Object.create(proto, [propertiesObject])
-// https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/Object/defineProperties
-// Object.create()方法创建一个新对象，使用现有的对象来提供新创建的对象的__proto__。 
+// https://msdn.microsoft.com/zh-cn/library/ff925952(v=vs.94).aspx
+/*
+ * prototype: 必需。  要用作原型的对象。  可以为 null。
+*/
 var ppl = Object.create(proto)
-console.log(ppl.__proto__ === proto)
+console.log(`ppl.__proto__ === proto: ${ppl.__proto__ === proto}`)
+console.log(ppl.sayHi())
 
-// application
+var somebody = Object.create(null)
+console.log(`somebody.__proto__: ${somebody.__proto__}`)
 
-function Shape (name) {
-  this.name = name
+
+// ES5 inheritance
+
+function Shape (x, y) {
+  this.x = x
+  this.y = y
 }
 
 Shape.prototype = {
-  sayName: function () {
-    console.log(this.name)
-  }
+  move: function (x, y) {
+    this.x += x
+    this.y += y
+    var name = this.name || ''
+    console.log(`${name} moved (${x}, ${y})`)
+  },
+  constructor: Shape
 }
 
-// inherit property from shape
-function Rectangle () {}
-Object.create(Rectangle.prototype, Shape.prototype)
+function Rectangle (x, y) {
+  Shape.call(this, x, y)
+  this.name = 'Rectangle'
+}
+
+Rectangle.prototype = Object.create(Shape.prototype)
+Rectangle.prototype.constructor = Rectangle
+
+console.log(`Rectangle.prototype.__proto__ === Shape.prototype: ${Rectangle.prototype.__proto__ === Shape.prototype}`)
+
+var rect = new Rectangle(10, 10)
+console.log(rect)
+rect.move(10, 20)
