8ec8efe8768f8a6a1cd9373097bc4329805a2282
zhang-xiao-miao <1286874200@qq.com>
2018-08-10 09:41:09 -0400

    static method for a class

---
+++ class/02.05.static method for class.js
@@ -0,0 +1,9 @@
+class A {
+  static staticMethod () {
+    console.log(`I'm a static method declared by static keyword`)
+  }
+
+  // static account // there is no static property for a class
+}
+
+A.staticMethod()
