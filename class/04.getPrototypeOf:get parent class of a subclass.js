531ab958b874ed8789c24693682b7d9e56bb5fd3
zhang-xiao-miao <1286874200@qq.com>
2018-08-10 09:26:57 -0400

    get prototype of an instance

--- class/04.getPrototypeOf:get parent class of a subclass.js
+++ class/04.getPrototypeOf:get parent class of a subclass.js
@@ -6,9 +6,16 @@ class Point {
 }
 
 class ColorPoint extends Point {
-  constructor (x, y) {
+  constructor (x, y, color) {
     super(x, y)
+    this.color = color
   }
 }
 
+// get ColorPoint's parent class
 console.log(`Object.getPrototypeOf(ColorPoint) === Point: ${Object.getPrototypeOf(ColorPoint) === Point}`)
+
+
+// get prototype of an instance
+const cPoint = new ColorPoint(10, 10, 'red')
+console.log(Object.getPrototypeOf(cPoint))
