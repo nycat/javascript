54a471665cb81c317f12bc0813d7824af57e2f25
zhang-xiao-miao <1286874200@qq.com>
2018-09-19 22:31:10 +0800

    a new context created for parameter which has a default value

--- function/11.es6_scope_for_default_value.js
+++ function/11.es6_scope_for_default_value.js
@@ -1,6 +1,7 @@
 /* example 1:
   scope for default value:
-  A new context created when there is a default value for a function. After declaration ended, the scope disappears.
+  When there is a default value for a function, there will be a newly created context, which will disappear when variable declaration ended.
+  This grammar behavior only exists when there is a default value for a parameter in a function declaration.
 */
 let x = 1
 function log(x, y = x) {
@@ -9,3 +10,13 @@ function log(x, y = x) {
 
 log(12)
 console.log(`x out of log function is: ${x}`)
+
+
+/*
+  For a function which has a default value of a parameter,  javascript engine creates a new context for its parameter.
+  As there is no x in the context created for parameter of foo, javascript engines looks it up to upper layer of the newly created context.
+*/
+function foo(y = x) {
+  console.log(`y is:${y}`)
+}
+foo()
